8b797c18703264ccc0de7388ae5d6559f993aa95
John Serrano <andrey_201015@hotmail.com>
2023-10-30 15:25:39 -0500

    Evitar hacer la búsqueda dos veces seguidas

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { type Movie } from '../types'
 import { searchMovies } from '../services/movies'
 
@@ -6,11 +6,17 @@ export function useMovies({ search }: { search: string }) {
   const [movies, setMovies] = useState<Movie[] | undefined>([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    // Al ser la misma movie (search) al intentar buscar evitamos que se haga la búsqueda dos veces seguidas.
+    // El useRef me guarda ese valor como referencia y no muta entre renderizados.
+    if (previousSearch.current === search) return
+
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (error) {
